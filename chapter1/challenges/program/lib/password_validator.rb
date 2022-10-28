dc70e60fcc1996b0fb1dabaeca2904ccfb24e169
Andrew <andynguyen93@hotmail.co.uk>
2022-10-28 17:36:55 +0100

    refactoring

--- chapter1/challenges/program/lib/password_validator.rb
+++ chapter1/challenges/program/lib/password_validator.rb
@@ -2,9 +2,11 @@
 # - 8 or more characters
 # - include at least one of these characters: !, @, $, %, &
 
+#def valid?(password)
+#  return password.length>7 && (password.include?("!") || password.include?("@") ||password.include?("£") ||password.include?("$") ||password.include?("%") || password.include?("&"))
+#end
+
 def valid?(password)
-  return password.length>7 && (password.include?("!") || password.include?("@") ||password.include?("£") ||password.include?("$") ||password.include?("%") || password.include?("&"))
+  return password.length>7 && password.match?(/[!@$%&]/)
 end
 
-
-#refactoring needed
